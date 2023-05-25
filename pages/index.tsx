e6534f74b9467ca3f945d72728b20c013c43f375
Nick Kramer <kramercodes@Nicks-MacBook-Air.local>
2023-05-25 11:08:09 -0400

    adds boards to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import { PrismaClient } from '@prisma/client'
+import { Card, Grid, Text } from "@nextui-org/react";
+import { PrismaClient, Board } from '@prisma/client'
 
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link';
 
 import styles from '@/pages/index.module.css'
 
@@ -17,6 +19,38 @@ export default function Home({boards}) {
         <h1 className={styles.title}>
           Welcome to Kanclone
         </h1>
+        <Grid.Container gap={2} justify="flex-start">
+          {boards.map((board: Board) => (
+            <Grid xs={6} sm={3} md={2}>
+              <Link href={`/boards/${board.id}`}>
+                <Card
+                  isPressable
+                  isHoverable
+                  variant="bordered"
+                  css={{ mw: "400px" }}
+                >
+                  <Card.Body>
+                    <Text>{board.title}</Text>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Grid>
+          ))}
+          <Grid xs={6} sm={3} md={2}>
+            <Link href={`/boards/new`}>
+              <Card
+                isPressable
+                isHoverable
+                variant="bordered"
+                css={{ mw: "400px" }}
+              >
+                <Card.Body>
+                  <Text>Create new board +</Text>
+                </Card.Body>
+              </Card>
+            </Link>
+          </Grid>
+        </Grid.Container>
       </main>
 
       <footer className={styles.footer}>
